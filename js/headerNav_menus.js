26ce2423cda9ef3310084c0dbc11b7ca1bfff1ba
Juan López <juanandrelopezcordova8@gmail.com>
2024-10-20 00:15:28 -0500

    header: adding menu prototype

---
+++ js/headerNav_menus.js
@@ -0,0 +1,10 @@
+// navsHeader Constants
+const navNosotros = document.getElementById("headerNav-nosotros");
+
+// menusHeader Constants
+const menuNosotros = document.getElementById("headerMenu-nosotros");
+
+// menus opening
+navNosotros.addEventListener("click", () => {
+    menuNosotros.showModal();
+})
