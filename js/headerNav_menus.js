5a61ef52f5b7500cea71c288eea34ca271d023ce
Juan López <juanandrelopezcordova8@gmail.com>
2024-10-22 02:48:37 -0500

    adding nosotros-menu-header

--- js/headerNav_menus.js
+++ js/headerNav_menus.js
@@ -8,3 +8,20 @@ const menuNosotros = document.getElementById("headerMenu-nosotros");
 navNosotros.addEventListener("click", () => {
     menuNosotros.showModal();
 })
+
+
+// work with linnks
+const buttonLinkNosotros = document.querySelector("#hm-nosotros-buttonImage a");
+const ubicacionSection = document.getElementById("ubicacion");
+
+buttonLinkNosotros.addEventListener("click", () => {
+    ubicacionSection.scrollIntoView({ behavior: 'smooth' });
+    menuNosotros.close();
+})
+
+// work with events (close with mouseleave)
+const divMenuNosotros = document.getElementById("hm-nosotros-all");
+
+divMenuNosotros.addEventListener("mouseleave", () => {
+    menuNosotros.close();
+})
